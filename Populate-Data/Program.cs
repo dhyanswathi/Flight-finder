a42bb4ba89470edbc74820573d9bf606e53e81b3
swathi <swathysidharthan@gmail.com>
2023-03-06 19:52:39 +0100

    create projects

---
+++ Populate-Data/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Populate_Data.Models;
+
+
+var cns = "Data Source=LAPTOP-AC42D6SP\\MSSQLSERVER01;Initial Catalog=FlightFinderDB;Integrated Security=True";
