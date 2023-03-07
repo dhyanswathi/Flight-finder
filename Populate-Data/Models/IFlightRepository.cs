29fda86ab6fe336199b0b3160436293ea88a3005
swathi <swathysidharthan@gmail.com>
2023-03-07 14:41:50 +0100

    add repository pattern

---
+++ Populate-Data/Models/IFlightRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Populate_Data.Models
+{
+    public interface IFlightRepository
+    {
+        void Save();
+        void CreateFlightRoute(string routeId, string depDestination, string arrDestination);
+        void CreateItinerary(string flightId, DateTime departure, DateTime arrival, int seats, string routeId);
+        void CreatePrices(string currency, double adult, double child, string flightId);
+    }
+}
--- Populate-Data/Program.cs
+++ Populate-Data/Program.cs
@@ -1,16 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.Hosting;
 using Populate_Data.Data;
 using Populate_Data.Models;
 
-
+IHost host = Host.CreateDefaultBuilder(args)
+.ConfigureServices((context, services) =>
+{
+    var cns = "Data Source=LAPTOP-AC42D6SP\\MSSQLSERVER01;Initial Catalog=MilkDB;Integrated Security=True;encrypt=false";
+    services.AddDbContext<MilkDBContext>(options => options.UseSqlServer(cns));
+    services.AddTransient<MilkRepository>();
+})
+.Build();
 var cns = "Data Source=LAPTOP-AC42D6SP\\MSSQLSERVER01;Initial Catalog=FlightFinderDB;Integrated Security=True";
 //var path = $"{Environment.CurrentDirectory}..\\..\\..\\..\\..\\data\\Flights.json";
 var path = "C:\\Users\\Mintu\\swathistudy\\pgp\\codeTest\\Flight-finder\\Populate-Data\\Data\\Flights.json";
 
 var items = JsonReader.GetFlightData(path);
-foreach (var item in items)
-{
-    Console.WriteLine(item.arrivalDestination);
-    Console.ReadLine();
-}
+
