fa2b26a395a25e3c08298ef8a63989260fe1f3c7
swathi <swathysidharthan@gmail.com>
2023-03-07 10:10:53 +0100

    create jsonreader method

--- Populate-Data/Data/JsonReader.cs
+++ Populate-Data/Data/JsonReader.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Populate_Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace Populate_Data.Data
 {
-    internal class JsonReader
+   public class JsonReader
     {
+        public class RouteResults
+        {
+            public List<FlightRoutes> Results { get; set; }
+        }
+
+        //Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public static List<FlightRoutes> GetFlightData(string path)
+        {
+            var json = File.ReadAllText(path);
+            var jsonFileFormat = JsonConvert.DeserializeObject<RouteResults>(json);
+            return jsonFileFormat.Results;
+        }
     }
 }
