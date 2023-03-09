481b1eb1bc15aab7e59e844ad308a2431944faea
swathi <swathysidharthan@gmail.com>
2023-03-09 21:49:07 +0100

    add interface ibookingrepository

---
+++ Flight-Finder.Api/DTO/BookingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Flight_Finder.Api.DTO
+{
+    public class BookingRequest
+    {
+        public int NumberOfSeats { get; set; }
+        public string FlightId { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ Flight-Finder.Api/Models/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using Flight_Finder.Api.DTO;
+
+namespace Flight_Finder.Api.Models
+{
+    public interface IBookingRepository
+    {
+        IEnumerable<Booking> GetAllBookings();
+        Booking GetBooking(string bookingId);
+        Booking CreateBooking(BookingRequest request);
+        void DeleteBooking(string bookingId);
+        void UpdateBooking(string bookingId, BookingRequest request);
+        void SaveBooking();
+    }
+}
