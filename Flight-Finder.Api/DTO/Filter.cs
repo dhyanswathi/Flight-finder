49598cdd18a793ec5f047c1760a471fd47cf8621
swathi <swathysidharthan@gmail.com>
2023-03-09 17:55:27 +0100

    add filter class

---
+++ Flight-Finder.Api/DTO/Filter.cs
@@ -0,0 +1,12 @@
+﻿namespace Flight_Finder.Api.DTO
+{
+    public class Filter
+    {
+        public string? DepartureLocation { get; set; }
+        public string? ArrivalLocation { get; set; }
+        public DateTime? TravelDate { get; set; }
+        public double? LowPrice { get; set; } = 0;
+        public double? HighPrice { get; set; }
+
+    }
+}
