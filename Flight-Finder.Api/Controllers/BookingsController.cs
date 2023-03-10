5cc5e5fc4f549e18412182b77f9123c6cc4514f5
swathi <swathysidharthan@gmail.com>
2023-03-10 10:58:40 +0100

    add httppost for booking

---
+++ Flight-Finder.Api/Controllers/BookingsController.cs
@@ -0,0 +1,26 @@
+﻿using Flight_Finder.Api.DTO;
+using Flight_Finder.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flight_Finder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly BookingRepository _bookingRepo;
+        public BookingsController(BookingRepository bookingRepo) 
+        {
+            _bookingRepo = bookingRepo;
+        }
+
+        [HttpPost]
+        public IActionResult AddBooking(BookingRequest request)
+        {
+            var result = _bookingRepo.CreateBooking(request);
+            return Created("", result);
+        }
+
+    }
+}
